bdb84666d15b027a22988855dc41a615ec702a0e
HaibullinDA <danilos10099@gmail.com>
2021-03-30 18:57:33 +0700

    commit

--- NotForgot/SignInViewController.swift
+++ NotForgot/SignInViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class SignInViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
