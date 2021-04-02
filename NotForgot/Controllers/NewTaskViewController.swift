09c862d4fd0bddd9d2967915ce8a6d0590ca1775
HaibullinDA <danilos10099@gmail.com>
2021-04-02 11:06:16 +0700

    createDatePicker

--- NotForgot/Controllers/NewTaskViewController.swift
+++ NotForgot/Controllers/NewTaskViewController.swift
@@ -16,23 +16,27 @@ class NewTaskViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     let cellIdentifire = "cellID"
+    let stringArray = ["Срок выполнения задачи","Категория задачи","Приоритет"]
     
-    @IBOutlet weak var datePickerView: UIView!
-    @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var buttonCloseInPicker: UIButton!
-    @IBOutlet weak var buttonOkInPicker: UIButton!
-    
+    let textField = UITextField()
+    let datePicker = UIDatePicker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        // Do any additional setup after loading the view.
+    }
+    
+    func setupView(){
         setupTitles()
         closeButton.addTarget(self, action: #selector(closeScreen(sender:)), for: .touchUpInside)
+        createDatePicker()
+        
         tableView.delegate = self
         tableView.dataSource = self
         tableView.isScrollEnabled = false
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifire)
         tableView.separatorStyle = .none
-        // Do any additional setup after loading the view.
     }
     
     @objc func closeScreen(sender: UIButton){
@@ -47,10 +51,36 @@ class NewTaskViewController: UIViewController {
         bottomLine.backgroundColor = UIColor.lightGray.cgColor
         descriptionTextView.layer.addSublayer(bottomLine)
     }
-    let stringArray = ["Срок выполнения задачи","Категория задачи","Приоритет"]
+    
+    func createToolbar() -> UIToolbar{
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed(sender:)))
+        
+        toolBar.setItems([doneButton], animated: true)
+        
+        return toolBar
+    }
+    
+    func createDatePicker(){
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.datePickerMode = .date
+        textField.inputView = datePicker
+        textField.inputAccessoryView = createToolbar()
+    }
+    
+    @objc func donePressed(sender: UIBarButtonItem){
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        
+        self.textField.text = dateFormatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
 }
 
-
+//MARK: -
 extension NewTaskViewController: UITableViewDelegate,UITableViewDataSource{
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -64,22 +94,23 @@ extension NewTaskViewController: UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         switch indexPath.row {
         case 0:
-            let cell = UITableViewCell(style: .value1, reuseIdentifier: cellIdentifire)
+            let cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifire)
             cell.textLabel?.text = stringArray[indexPath.row]
             cell.layer.borderWidth = 0.5
             cell.layer.borderColor = UIColor.lightGray.cgColor
-            cell.detailTextLabel?.text = "Sep 17, 2018"
-            cell.detailTextLabel?.textColor = .lightGray
+            textField.frame = CGRect(x: cell.frame.width-20, y: cell.frame.minY + 5, width: cell.frame.width/3, height: cell.contentView.frame.height)
+            textField.textColor = .lightGray
+            cell.contentView.addSubview(textField)
             return cell
         case 1:
-            let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifire, for: indexPath)
+            let cell = UITableViewCell(style: .value1, reuseIdentifier: cellIdentifire)
             cell.textLabel?.text = stringArray[indexPath.row]
             cell.layer.borderWidth = 0.5
             cell.layer.borderColor = UIColor.lightGray.cgColor
             cell.accessoryType = .disclosureIndicator
             return cell
         case 2:
-            let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifire, for: indexPath)
+            let cell = UITableViewCell(style: .value1, reuseIdentifier: cellIdentifire)
             cell.textLabel?.text = stringArray[indexPath.row]
             cell.layer.borderWidth = 0.5
             cell.layer.borderColor = UIColor.lightGray.cgColor
@@ -95,36 +126,8 @@ extension NewTaskViewController: UITableViewDelegate,UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.row == 0 {
-            self.datePickerView.isHidden = true
-            self.buttonCloseInPicker.isEnabled = true
-            self.buttonOkInPicker.isEnabled = true
-            self.buttonCloseInPicker.addTarget(self, action: #selector(closePicker(sender:)), for: .touchUpInside)
-            //self.buttonOkInPicker.addTarget(self, action: #selector(okPicker(sender:)), for: .touchUpInside)
-        }
+
     }
     
-    func pickerSetup(){
-        self.datePickerView.isHidden = false
-        self.buttonCloseInPicker.isEnabled = false
-        self.buttonOkInPicker.isEnabled = false
-        
-    }
     
-    func addTatgetInPicker(){
-        
-    }
-    
-    @objc func closePicker(sender: UIButton){
-        pickerSetup()
-    }
-    /*
-    @objc func okPicker(sender: Any?){
-        pickerSetup()
-        if let indexPath = sender as? IndexPath{
-            tableView.cellForRow(at: indexPath)?.detailTextLabel?.text = String(self.datePicker.
-            
-        }
-        
-    }*/
 }
